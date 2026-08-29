36a3fb25e310a631e029d4289c5ce6eeccfe66b0
Roberevan224 <roberevan224@gmail.com>
2026-08-29 12:21:13 -0500

    Add browser commands to TAWS preload bridge

--- ui-preload.js
+++ ui-preload.js
@@ -1,11 +1,14 @@
 const { contextBridge, ipcRenderer } = require('electron');
 const allowed = new Set(['taws:created','taws:tab','taws:active','taws:closed','taws:state','taws:history','taws:error','taws:download','taws:setup']);
 contextBridge.exposeInMainWorld('taws', {
-  newTab: (url) => ipcRenderer.invoke('taws:new-tab', url), activate: (id) => ipcRenderer.invoke('taws:activate', id),
-  navigate: (url) => ipcRenderer.invoke('taws:navigate', url), back: () => ipcRenderer.invoke('taws:back'),
-  forward: () => ipcRenderer.invoke('taws:forward'), reload: () => ipcRenderer.invoke('taws:reload'), stop: () => ipcRenderer.invoke('taws:stop'),
+  newTab: (url) => ipcRenderer.invoke('taws:new-tab', url), activate: (id) => ipcRenderer.invoke('taws:activate', id), navigate: (url) => ipcRenderer.invoke('taws:navigate', url),
+  back: () => ipcRenderer.invoke('taws:back'), forward: () => ipcRenderer.invoke('taws:forward'), reload: () => ipcRenderer.invoke('taws:reload'), stop: () => ipcRenderer.invoke('taws:stop'),
   closeTab: (id) => ipcRenderer.invoke('taws:close-tab', id), home: () => ipcRenderer.invoke('taws:home'), devtools: () => ipcRenderer.invoke('taws:devtools'),
   openExternal: (url) => ipcRenderer.invoke('taws:external', url), loadVault: () => ipcRenderer.invoke('taws:vault-load'), saveVault: (value) => ipcRenderer.invoke('taws:vault-save', value),
-  loadSetup: () => ipcRenderer.invoke('taws:setup-load'), saveSetup: (value) => ipcRenderer.invoke('taws:setup-save', value)
+  loadSetup: () => ipcRenderer.invoke('taws:setup-load'), saveSetup: (value) => ipcRenderer.invoke('taws:setup-save', value),
+  find: (text) => ipcRenderer.invoke('taws:find', text), stopFind: () => ipcRenderer.invoke('taws:stop-find'),
+  zoomIn: () => ipcRenderer.invoke('taws:zoom-in'), zoomOut: () => ipcRenderer.invoke('taws:zoom-out'), zoomReset: () => ipcRenderer.invoke('taws:zoom-reset'),
+  print: () => ipcRenderer.invoke('taws:print'), savePage: () => ipcRenderer.invoke('taws:save-page'), newWindow: (url) => ipcRenderer.invoke('taws:new-window', url),
+  fullscreen: () => ipcRenderer.invoke('taws:fullscreen')
 });
 for (const channel of allowed) ipcRenderer.on(channel, (_event, data) => window.dispatchEvent(new CustomEvent(channel, { detail: data })));
